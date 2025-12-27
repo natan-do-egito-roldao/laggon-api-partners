b09949bf39c78b667df3ff7b2f03110e281c01ba
Natan.sys <eusuez@gmail.com>
2025-12-27 12:25:57 -0300

    atualizando cors novamente

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// Fallback preflight handler: set CORS headers and respond to OPTIONS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
 // ✅ depois JSON
 app.use(express.json());
 
